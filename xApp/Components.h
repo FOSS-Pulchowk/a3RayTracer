ab8f7a39430d65a35414deb148cdf3ea816e6961
Zero <zeroa5620@gmail.com>
2019-02-05 22:30:33 +0545

    aman's code integrated into the application. thank you aman :thumps_up:

--- xApp/Components.h
+++ xApp/Components.h
@@ -1,4 +1,7 @@
+// STL's !!!!
 #include <vector>
+#include <string>
+
 struct cpu {
 	std::string name;
 	float speed;
@@ -62,48 +65,4 @@ struct powerSupply {
 struct casing{
 	std::string name, type;
 	float price;
-	//casing(std::string a, std::string b, float c ):name(a), type(b),price(c){}
-};
-
-std::vector<casing> vCase;
-std::vector<powerSupply> vPowerSupply;
-std::vector<videoCard> vVideoCard;
-std::vector<storage> vStorage;
-std::vector<memory> vMemory;
-std::vector<cpuCooler> vCpuCooler;
-std::vector<motherboard> vMotherboard;
-std::vector<cpu> vCpu;
-
-void setComponents() {
-	vCase.push_back(casing{ "Thermaltake Core G3","ATX Mid Tower",69.49 });
-	vCase.push_back(casing{ "Lian-Li PC-O10 WX","ATX Mid Tower",249.99 });
-
-	vPowerSupply.push_back(powerSupply{"EVGA SuperNOVA 750","SuperNOVA G3","ATX","80+ Gold",750,"Full",4.5,99.89});
-	vPowerSupply.push_back(powerSupply{"Corsair CX550M","CXM","ATX","80+ Bronze",550,"Semi",4.5,33.99});
-	vPowerSupply.push_back(powerSupply{"Corsair TX650M Gold","TXM Gold","ATX","80+ Gold",650,"Semi",4.5,39.89});
-
-	vVideoCard.push_back(videoCard{"MSI GeForce GTX 1060 6GT OCV1","GT OCV1","GeForce GTX 1060 6GB",6,1.544,4.5, 529.89});
-	vVideoCard.push_back(videoCard{"Asus DUAL-GTX1070-O8G","Dual Series","GeForce GTX 1070",8,1.582,4.5, 439.89});
-	vVideoCard.push_back(videoCard{"Gigabyte GV-RX570AORUS-4GD","AORUS 4G","Radeon RX 570",4,1.168,4, 139.99});
-
-	vStorage.push_back(storage{"Western Digital WD10EZEX","Caviar Blue","3.5","7200 RPM","1 TB",64,4.5,44.89});
-	vStorage.push_back(storage{"Samsung MZ-76E500B/AM","860 Evo","2.5","SSD","500 GB",512,4,82.99});
-	vStorage.push_back(storage{"Intel SSDPECME040T401","DC P3608","PCI-E","SSD","4 TB",0,0,8867.99});
-
-	vMemory.push_back(memory{ "Corsair Vengeance LPX","DDR4-3000","288-pin DIMM",15,2,8,16,4.5,114.99 });
-	vMemory.push_back(memory{ "G.Skill Aegis","DDR4-3000","288-pin DIMM",15,2,8,16,4.5,94.99 });
-	vMemory.push_back(memory{ "Team Vulcan","DDR4-3000","288-pin DIMM",15,2,4,8,4.5,60.88 });
-
-	vCpuCooler.push_back(cpuCooler{ "Cooler Master Hyper 212 EVO",600,2000,36,4.5,27.89 });
-	vCpuCooler.push_back(cpuCooler{"Cooler Master Hyper T2",0,2800,35,4,16.89});
-	vCpuCooler.push_back(cpuCooler{ "be quiet! Dark Rock 4",0,1400,35,4.5,68.99 });
-
-	vCpu.push_back(cpu{ "Intel Core i7 - 8700K",3.7,6,95,4.5,369.99});
-	vCpu.push_back(cpu{ "Intel Core i9 - 9900K",3.6,8,95,4.5,529.99});
-	vCpu.push_back(cpu{ "Intel Core i5 - 9600K",3.7,6,95,4.5,259.99});
-
-	vMotherboard.push_back(motherboard{"MSI Z370-A PRO","LGA1151","ATX",4,64,4,109.89});
-	vMotherboard.push_back(motherboard{"Asus PRIME Z390-A","LGA1151","ATX",4,64,0,183.00});
-	vMotherboard.push_back(motherboard{"Gigabyte H310M A","LGA1151","Micro ATX",2,32,5,55.99});
-	
 };
--- xApp/a3Interface.h
+++ xApp/a3Interface.h
@@ -14,9 +14,7 @@
 #include "Platform/GLResources.h"
 #include "Utility/Resource.h"
 
-#include <string>
-#include <vector>
-using namespace std;
+#include "Components.h"
 
 #define ADD_COMPONENT(x) struct x { string name; }
 
@@ -43,29 +41,20 @@ private:
 		Case,
 		Total
 	};
-	ADD_COMPONENT(motherboard);
-	ADD_COMPONENT(processor);
-	ADD_COMPONENT(memory);
-	ADD_COMPONENT(cooler);
-	ADD_COMPONENT(storage);
-	ADD_COMPONENT(video_card);
-	ADD_COMPONENT(power_supply);
-	ADD_COMPONENT(pc_case);
-
 	a3::ui_context uiContext;
 	a3::font_renderer fontRenderer;
 	a3::basic2d_renderer renderer2D;
 
-	pc_components currentlyOpen;
+	std::vector<casing> vCase;
+	std::vector<powerSupply> vPowerSupply;
+	std::vector<videoCard> vVideoCard;
+	std::vector<storage> vStorage;
+	std::vector<memory> vMemory;
+	std::vector<cpuCooler> vCpuCooler;
+	std::vector<motherboard> vMotherboard;
+	std::vector<cpu> vCpu;
 
-	vector<motherboard> vMotherBoards;
-	vector<processor> vProcessors;
-	vector<memory> vMemories;
-	vector<cooler> vCoolers;
-	vector<storage> vStorages;
-	vector<video_card> vVideoCards;
-	vector<power_supply> vPowerSupplies;
-	vector<pc_case> vCases;
+	pc_components currentlyOpen;
 
 	static const char* s_ComponentNames[pc_components::Total - 1];
 
@@ -76,6 +65,7 @@ public:
 	void Render();
 
 private:
+	void SetComponents();
 	void RenderUI(v2 position, v2 dimension);
 	void RenderComponentWindow(pc_components c);
 };
@@ -110,8 +100,9 @@ void App::Init()
 	fontRenderer.SetRegion(0.0f, 1280.0f, 0.0f, 720.0f);
 	fontRenderer.SetFont(a3::Asset.Get<a3::font_texture>(a3::asset_id::UserFont));
 
-
 	renderer2D.SetRegion(0.0f, 1280.0f, 0.0f, 720.0f);
+
+	SetComponents();
 }
 
 void App::Update(const a3::input_info& input)
@@ -131,6 +122,40 @@ void App::Render()
 	RenderUI({ 1050.0f, 600.0f }, { 200.0f, 50.0f });
 	RenderComponentWindow(currentlyOpen);
 }
+
+void App::SetComponents()
+{
+	vCase.push_back(casing{ "Thermaltake Core G3","ATX Mid Tower",69.49 });
+	vCase.push_back(casing{ "Lian-Li PC-O10 WX","ATX Mid Tower",249.99 });
+
+	vPowerSupply.push_back(powerSupply{ "EVGA SuperNOVA 750","SuperNOVA G3","ATX","80+ Gold",750,"Full",4.5,99.89 });
+	vPowerSupply.push_back(powerSupply{ "Corsair CX550M","CXM","ATX","80+ Bronze",550,"Semi",4.5,33.99 });
+	vPowerSupply.push_back(powerSupply{ "Corsair TX650M Gold","TXM Gold","ATX","80+ Gold",650,"Semi",4.5,39.89 });
+
+	vVideoCard.push_back(videoCard{ "MSI GeForce GTX 1060 6GT OCV1","GT OCV1","GeForce GTX 1060 6GB",6,1.544,4.5, 529.89 });
+	vVideoCard.push_back(videoCard{ "Asus DUAL-GTX1070-O8G","Dual Series","GeForce GTX 1070",8,1.582,4.5, 439.89 });
+	vVideoCard.push_back(videoCard{ "Gigabyte GV-RX570AORUS-4GD","AORUS 4G","Radeon RX 570",4,1.168,4, 139.99 });
+
+	vStorage.push_back(storage{ "Western Digital WD10EZEX","Caviar Blue","3.5","7200 RPM","1 TB",64,4.5,44.89 });
+	vStorage.push_back(storage{ "Samsung MZ-76E500B/AM","860 Evo","2.5","SSD","500 GB",512,4,82.99 });
+	vStorage.push_back(storage{ "Intel SSDPECME040T401","DC P3608","PCI-E","SSD","4 TB",0,0,8867.99 });
+
+	vMemory.push_back(memory{ "Corsair Vengeance LPX","DDR4-3000","288-pin DIMM",15,2,8,16,4.5,114.99 });
+	vMemory.push_back(memory{ "G.Skill Aegis","DDR4-3000","288-pin DIMM",15,2,8,16,4.5,94.99 });
+	vMemory.push_back(memory{ "Team Vulcan","DDR4-3000","288-pin DIMM",15,2,4,8,4.5,60.88 });
+
+	vCpuCooler.push_back(cpuCooler{ "Cooler Master Hyper 212 EVO",600,2000,36,4.5,27.89 });
+	vCpuCooler.push_back(cpuCooler{ "Cooler Master Hyper T2",0,2800,35,4,16.89 });
+	vCpuCooler.push_back(cpuCooler{ "be quiet! Dark Rock 4",0,1400,35,4.5,68.99 });
+
+	vCpu.push_back(cpu{ "Intel Core i7 - 8700K",3.7,6,95,4.5,369.99 });
+	vCpu.push_back(cpu{ "Intel Core i9 - 9900K",3.6,8,95,4.5,529.99 });
+	vCpu.push_back(cpu{ "Intel Core i5 - 9600K",3.7,6,95,4.5,259.99 });
+
+	vMotherboard.push_back(motherboard{ "MSI Z370-A PRO","LGA1151","ATX",4,64,4,109.89 });
+	vMotherboard.push_back(motherboard{ "Asus PRIME Z390-A","LGA1151","ATX",4,64,0,183.00 });
+	vMotherboard.push_back(motherboard{ "Gigabyte H310M A","LGA1151","Micro ATX",2,32,5,55.99 });
+};
 
 void App::RenderUI(v2 position, v2 dimension)
 {
