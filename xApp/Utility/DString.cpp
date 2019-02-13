9a04acd2596ac6e56f38c427c33753f590af7dc0
Zero <zeroa5620@gmail.com>
2019-02-13 23:19:19 +0545

    Added dstring

---
+++ xApp/Utility/DString.cpp
@@ -0,0 +1,149 @@
+#include "DString.h"
+#include "String.h"
+
+namespace a3
+{
+
+	dstring::dstring() :
+		m_Length(0),
+		m_Data(0)
+	{
+	}
+
+	dstring::dstring(const char* cstr) :
+		m_Length(a3::GetStringLength(cstr) + 1),
+		m_Data(a3Malloc(m_Length, char))
+	{
+		a3::MemoryCopy(m_Data, cstr, m_Length);
+	}
+
+	dstring::dstring(dstring& str)
+	{
+		Move(*this, str);
+	}
+
+	dstring::~dstring()
+	{
+		a3Free(m_Data);
+	}
+
+	void dstring::New(u64 len)
+	{
+		m_Length = len;
+		m_Data = a3Realloc(m_Data, len, char);
+	}
+
+	void dstring::Delete()
+	{
+		m_Length = 0;
+		a3Free(m_Data);
+		m_Data = A3NULL;
+	}
+
+	void dstring::Move(dstring & l, dstring & r)
+	{
+		l.m_Length = r.m_Length;
+		l.m_Data = r.m_Data;
+		r.m_Length = 0;
+		r.m_Data = A3NULL;
+	}
+
+	void dstring::Copy(dstring & l, dstring & r)
+	{
+		l.m_Length = r.m_Length;
+		l.m_Data = a3Realloc(l.m_Data, l.m_Length, char);
+		a3::MemoryCopy(l.m_Data, r.m_Data, r.m_Length);
+	}
+
+	dstring& dstring::operator=(const char* cstr)
+	{
+		m_Length = a3::GetStringLength(cstr) + 1;
+		m_Data = a3Realloc(m_Data, m_Length, char);
+		a3::MemoryCopy(m_Data, cstr, m_Length);
+		m_Length = a3::GetStringLength(cstr) + 1;
+		return *this;
+	}
+
+	dstring& dstring::operator=(dstring& str)
+	{
+		Move(*this, str);
+		return *this;
+	}
+
+	char& dstring::operator[](u64 i)
+	{
+		return m_Data[i];
+	}
+
+	const char& dstring::operator[](u64 i) const
+	{
+		return m_Data[i];
+	}
+
+	char* dstring::CharArray()
+	{
+		return m_Data;
+	}
+
+	const char* dstring::CharArray() const
+	{
+		return m_Data;
+	}
+
+	u64 dstring::Length() const
+	{
+		return m_Length;
+	}
+
+	dstring& dstring::ToUpper(dstring & str)
+	{
+		for (u64 i = 0; i < str.m_Length; i++)
+		{
+			if (str.m_Data[i] > 96 && str.m_Data[i] < 123)
+				str.m_Data[i] -= 32;
+		}
+		return str;
+	}
+
+	dstring& dstring::ToLower(dstring & str)
+	{
+		for (u64 i = 0; i < str.m_Length; i++)
+		{
+			if (str.m_Data[i] > 64 && str.m_Data[i] < 91)
+				str.m_Data[i] += 32;
+		}
+		return str;
+	}
+
+	bool operator==(const dstring& one, const dstring& other)
+	{
+		if (one.m_Length != other.m_Length) return false;
+		for (u64 i = 0; i < one.m_Length; i++)
+			if (one.m_Data[i] != other.m_Data[i])
+				return false;
+		return true;
+	}
+
+	bool operator!=(const dstring& one, const dstring& other)
+	{
+		return !(one == other);
+	}
+
+	dstring operator+(const dstring& lhs, const dstring& rhs)
+	{
+		dstring temp;
+		temp.New((lhs.m_Length + rhs.m_Length - 1));
+		a3::MemoryCopy(temp.m_Data, lhs.m_Data, (lhs.m_Length) * sizeof(char));
+		a3::MemoryCopy(&temp.m_Data[0], rhs.m_Data, rhs.m_Length * sizeof(char));
+		return temp;
+	}
+
+	dstring& dstring::operator+=(const dstring& other)
+	{
+		m_Data = a3Realloc(m_Data, (m_Length + other.m_Length - 1), char);
+		a3::MemoryCopy(&m_Data[m_Length - 1], other.m_Data, other.m_Length * sizeof(char));
+		m_Length = m_Length + other.m_Length - 1;
+		return *this;
+	}
+
+}
---
+++ xApp/Utility/DString.h
@@ -0,0 +1,53 @@
+#include "Common/Core.h"
+#include "Platform/Platform.h"
+
+namespace a3
+{
+	// NOTE(Zero):
+	// The dstring class is different from std::class
+	// dstring does not copy or have assignment
+	// dstring always moves and so the ownership of the class must be carefully maintained
+	// this is done to make the code run faster
+
+	struct dstring
+	{
+	private:
+		u64 m_Length;
+		char* m_Data;
+
+	public:
+		dstring();
+		dstring(const char* cstr);
+		dstring(dstring& str);
+		~dstring();
+
+		void New(u64 len);
+		void Delete();
+		void Move(dstring& l, dstring& r); // owndership is transfered DEFAULT!
+		void Copy(dstring& l, dstring& r);
+
+		dstring& operator=(const char* cstr);
+		dstring& operator=(dstring& str);
+
+		char& operator[](u64 i);
+		const char& operator[](u64 i) const;
+
+		char* CharArray();
+		const char* CharArray() const;
+
+		u64 Length() const;
+
+		dstring& ToUpper(dstring& str);
+		dstring& ToLower(dstring& str);
+
+		friend bool operator==(const dstring& one, const dstring& other);
+		friend bool operator!=(const dstring& one, const dstring& other);
+
+		// lhs and rhs remain as they are, ownership is not transfered
+		friend dstring operator+(const dstring& lhs, const dstring& rhs); 
+
+		// other remains as it is, ownership is not transfered
+		dstring& operator+=(const dstring& other);
+	};
+
+}
